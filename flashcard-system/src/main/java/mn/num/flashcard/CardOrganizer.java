e5e214743b729f8d46a2c169b811972f7970f6ab
Tugu8 <tugutugu018@gmail.com>
2026-04-20 09:18:08 +0800

    Add documentation comment to CardOrganizer interface

--- flashcard-system/src/main/java/mn/num/flashcard/CardOrganizer.java
+++ flashcard-system/src/main/java/mn/num/flashcard/CardOrganizer.java
@@ -2,6 +2,9 @@ package mn.num.flashcard;
 
 import java.util.List;
 
+/**
+ * Interface for organizing cards in different orders.
+ */
 public interface CardOrganizer {
     List<Card> organize(List<Card> cards);
 }
